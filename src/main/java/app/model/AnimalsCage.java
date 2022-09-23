5a5640c92127e867eaf07d067f537d854e4b7f7a
вашÐJuly000M <julim2131@gmail.com>
2022-09-23 20:39:38 +0300

    Done

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,27 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
-    private Animal animal;
+    private final Animal dog;
+    private final Timer timer;
+
+    public AnimalsCage(Animal dog, Timer timer) {
+        this.dog = dog;
+        this.timer = timer;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(dog.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
 }
